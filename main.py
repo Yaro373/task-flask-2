95724642c4b2e8fd1ecf4de9dfd90c6befa5760b
Yaro373 <zakoyasse@gmail.com>
2022-02-15 18:19:58 +0300

    task4

--- main.py
+++ main.py
@@ -33,5 +33,19 @@ def list_prof(type):
     ])
 
 
+@app.route('/auto_answer')
+def auto_answer():
+    return render_template('auto_answer.html', values={
+        'title': 'Анкета',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'выше среднего',
+        'profession': 'штурман марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True',
+    })
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
